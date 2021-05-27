b9756f3ce534ffff84d817df980b6d62841db03c
Midasa <samimitwalli@gmail.com>
2021-05-27 12:10:09 +0200

    added dependencies, ide-test, and more examples

--- editor/backend/releng/de.jabc.cinco.meta.core.parent/de.jabc.cinco.meta.productdefinition.ide/src/test/java/de/jabc/cinco/meta/productdefinition/ide/test/CustomEndpointTest.java
+++ editor/backend/releng/de.jabc.cinco.meta.core.parent/de.jabc.cinco.meta.productdefinition.ide/src/test/java/de/jabc/cinco/meta/productdefinition/ide/test/CustomEndpointTest.java
@@ -1,14 +1,55 @@
 package de.jabc.cinco.meta.productdefinition.ide.test;
 
+import java.io.File;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.testing.AbstractLanguageServerTest;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import com.google.inject.Inject;
+import de.jabc.cinco.meta.core.utils.WorkspaceContext;
+import de.jabc.cinco.meta.productdefinition.ide.endpoint.CincoLanguageServerExtension;
 
-class CustomEndpointTest {
+class CustomEndpointTest extends AbstractLanguageServerTest {
 	
+	@Inject
+	CincoLanguageServerExtension cincoLse;
+	
+	public CustomEndpointTest() {
+		super("mgl");
+	}
+
 	@Test
-	@DisplayName("Custom Endpoint Test")
-	void testCustomEndpoint() {
-		Assertions.assertTrue(true);
+	void testWorkspaceContext() {
+		URI base = URI.createURI(this.root.toURI().toString());
+		base = base.trimSegments(1).appendSegment("resources");
+		String baseString = base.toString();
+		
+		WorkspaceContext workspaceContext = new WorkspaceContext(baseString, null);
+		String result = workspaceContext.getRootURI().toString();
+		String expected = base.toString();
+		Assertions.assertTrue(result.equals(expected));
+		
+		URI fileURI = workspaceContext.getFileURI(baseString);
+		Assertions.assertTrue(fileURI.toString().equals(baseString));
+
+		File baseFile = workspaceContext.getRootFile();
+		File baseFile2 = workspaceContext.getFile(workspaceContext.getRootURI());
+		Assertions.assertTrue(baseFile.getPath().equals(baseFile2.getPath()));
+		
+		String testFileName = "FlowGraph.mgl";
+		URI testFileURI = base.appendSegment(testFileName);
+		File testFile = workspaceContext.getFile(testFileURI);
+		File folder = workspaceContext.getFolder(testFile);
+		boolean exists = workspaceContext.fileExists(base);
+		boolean testFileExists = workspaceContext.fileExists(testFileURI);
+		Assertions.assertTrue(folder.getPath().equals(baseFile.getPath()));
+
+		Assertions.assertTrue(exists);
+		Assertions.assertTrue(testFileExists);
+		Assertions.assertTrue(workspaceContext.isContainedInRoot(testFile));
+		Assertions.assertTrue(workspaceContext.containsOrIsSame(folder, testFile));
+		
+		String name = workspaceContext.getRootFolderName();
+		Assertions.assertTrue(name.equals("resources"));
 	}
 }
