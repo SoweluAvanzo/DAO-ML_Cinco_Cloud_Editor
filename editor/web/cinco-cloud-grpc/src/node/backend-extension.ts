50ad13147381549b722473534fc2f921d9bf55dd
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2021-07-01 13:48:56 +0200

    move grpc editor stuff to cinco-extension

--- editor/web/cinco-cloud-grpc/src/node/backend-extension.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable header/header */
-import { BackendApplicationContribution } from '@theia/core/lib/node/backend-application';
-import { ContainerModule } from 'inversify';
-import { CincoCloudGrpc } from './cinco-cloud-grpc';
-
-export default new ContainerModule(bind => {
-    bind(BackendApplicationContribution).to(CincoCloudGrpc).inSingletonScope();
-});
