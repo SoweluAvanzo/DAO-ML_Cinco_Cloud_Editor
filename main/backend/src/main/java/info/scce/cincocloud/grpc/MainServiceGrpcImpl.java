51baa0d9010dc6fcb78b4da18cf7956f17ab259b
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2021-07-02 15:51:50 +0200

    main: update grpc service

--- main/backend/src/main/java/info/scce/cincocloud/grpc/MainServiceGrpcImpl.java
+++ main/backend/src/main/java/info/scce/cincocloud/grpc/MainServiceGrpcImpl.java
@@ -16,20 +16,26 @@ public class MainServiceGrpcImpl extends MutinyMainServiceGrpc.MainServiceImplBa
         final var projectId = request.getProjectId();
         final var archiveInBytes = request.getArchive();
 
+        if (projectId < 0) {
+            return Uni.createFrom().item(createImageReply(projectId, false, "projectId must be > 0"));
+        } else if (archiveInBytes.isEmpty()) {
+            return Uni.createFrom().item(createImageReply(projectId, false, "archive must not be empty"));
+        }
+
         final var projectOptional = PyroProjectDB.findByIdOptional(projectId);
         if (projectOptional.isEmpty()) {
-            return Uni.createFrom().item(CincoCloudProtos.CreateImageReply.newBuilder()
-                    .setProjectId(projectId)
-                    .setSuccess(false)
-                    .setMessage("project not found")
-                    .build());
+            return Uni.createFrom().item(createImageReply(projectId, false, "project not found"));
         } else {
             // TODO: save archive, send build job to artemis mq
-            return Uni.createFrom().item(CincoCloudProtos.CreateImageReply.newBuilder()
-                    .setProjectId(projectId)
-                    .setSuccess(true)
-                    .setMessage("success")
-                    .build());
+            return Uni.createFrom().item(createImageReply(projectId, true, "success"));
         }
+    }
+
+    private CincoCloudProtos.CreateImageReply createImageReply(long projectId, boolean success, String message) {
+        return CincoCloudProtos.CreateImageReply.newBuilder()
+                .setProjectId(projectId)
+                .setSuccess(success)
+                .setMessage(message)
+                .build();
     }
 }
