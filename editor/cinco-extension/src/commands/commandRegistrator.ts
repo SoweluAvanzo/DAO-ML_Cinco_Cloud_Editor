1f6b9639322d96bc8b712c73abc1c22950a5989d
midasa <samimitwalli@gmail.com>
2021-07-09 12:10:11 +0200

    added generationRequest information

--- editor/cinco-extension/src/commands/commandRegistrator.ts
+++ editor/cinco-extension/src/commands/commandRegistrator.ts
@@ -1,6 +1,9 @@
-import { commands, Disposable, ExtensionContext, window } from "vscode";
+import { commands, Disposable, ExtensionContext, window, workspace } from "vscode";
+import * as vscode from 'vscode';
 import { CincoLanguageClient } from "../lsp/cincoLanguageClient";
 import { generationCommandId } from '../registry/commandRegistry';
+import { WorkspaceFoldersFeature } from "vscode-languageclient/lib/workspaceFolders";
+import { relative } from "path";
 
 export function registerGenerationCommand(context: ExtensionContext, languageClient: CincoLanguageClient) {
     const generationCmd: Disposable = commands.registerCommand(generationCommandId, () => generationCommand(languageClient));
@@ -8,10 +11,22 @@ export function registerGenerationCommand(context: ExtensionContext, languageCli
 }
 
 async function generationCommand(languageClient: CincoLanguageClient) {
-    // TODO: get sourceUri of cpd-document
-    const sourceUri: string = "A";
+    const document = window.activeTextEditor.document;
+    const sourceUri = document.uri.fsPath;
+    const relativePath = workspace.asRelativePath(sourceUri, false);
+    const folder = workspace.workspaceFolders.find((value, index, obj) => {
+        try {
+            const recreated = vscode.Uri.joinPath(value.uri, relativePath).fsPath;
+            return recreated == sourceUri;
+        } catch(e) {
+            return false;
+        }
+    });
+    if(!folder) {
+        window.showErrorMessage("file needs to be in a workspace folder.");
+    }
     // TODO: get targetUri for generation path (static?)
-    const targetUri: string = "B";
+    const targetUri: string = folder.uri.fsPath;
 
     if(languageClient) {
         let value: string = await window.showInformationMessage(
