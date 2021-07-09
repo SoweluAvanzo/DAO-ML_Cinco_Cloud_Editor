2ffdb68c05033aeaab460dae9fcac3dc1eb5b933
midasa <samimitwalli@gmail.com>
2021-07-09 13:09:23 +0200

    added sending generatorRequest info

--- editor/cinco-extension/src/commands/commandRegistrator.ts
+++ editor/cinco-extension/src/commands/commandRegistrator.ts
@@ -1,9 +1,7 @@
-import { commands, Disposable, ExtensionContext, window, workspace } from "vscode";
-import * as vscode from 'vscode';
-import { CincoLanguageClient } from "../lsp/cincoLanguageClient";
+import { commands, Disposable, ExtensionContext, window, workspace } from 'vscode';
+
+import { CincoLanguageClient } from '../lsp/cincoLanguageClient';
 import { generationCommandId } from '../registry/commandRegistry';
-import { WorkspaceFoldersFeature } from "vscode-languageclient/lib/workspaceFolders";
-import { relative } from "path";
 
 export function registerGenerationCommand(context: ExtensionContext, languageClient: CincoLanguageClient) {
     const generationCmd: Disposable = commands.registerCommand(generationCommandId, () => generationCommand(languageClient));
@@ -13,26 +11,20 @@ export function registerGenerationCommand(context: ExtensionContext, languageCli
 async function generationCommand(languageClient: CincoLanguageClient) {
     const document = window.activeTextEditor.document;
     const sourceUri = document.uri.fsPath;
-    const relativePath = workspace.asRelativePath(sourceUri, false);
-    const folder = workspace.workspaceFolders.find((value, index, obj) => {
-        try {
-            const recreated = vscode.Uri.joinPath(value.uri, relativePath).fsPath;
-            return recreated == sourceUri;
-        } catch(e) {
-            return false;
-        }
-    });
-    if(!folder) {
-        window.showErrorMessage("file needs to be in a workspace folder.");
+    console.log("Generating - sourceURI: " + sourceUri);
+    const folder = workspace.getWorkspaceFolder(document.uri);
+    if (!folder) {
+        window.showErrorMessage(document.fileName + " needs to be in a workspace folder.");
     }
     // TODO: get targetUri for generation path (static?)
     const targetUri: string = folder.uri.fsPath;
+    console.log("Generating - targetURI: " + targetUri);
 
-    if(languageClient) {
+    if (languageClient) {
         let value: string = await window.showInformationMessage(
             "Do you want to run the generated Pyro Product?",
             ...["Yes", "No", "Cancel"]);
-        if(value=="Cancel")
+        if (value == "Cancel")
             return;
         // trigger generation
         languageClient.generate(sourceUri, targetUri, value == "Yes");
