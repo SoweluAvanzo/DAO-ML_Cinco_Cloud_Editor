3b1c3612dd468bb645dec284aa8dffa65fc95c09
midasa <samimitwalli@gmail.com>
2021-07-16 03:42:46 +0200

    updated cinco-authenticator

--- editor/web/cinco-frontend-configurator/src/browser/frontend-extension.ts
+++ editor/web/cinco-frontend-configurator/src/browser/frontend-extension.ts
@@ -1,9 +1,9 @@
 /* eslint-disable header/header */
-
-import { ContainerModule, interfaces } from 'inversify';
 import { MenuContribution } from '@theia/core';
-import { MenuCommandRemovalContribution } from './menu-command-removal-contribution';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
+import { ContainerModule, interfaces } from 'inversify';
+
+import { MenuCommandRemovalContribution } from './menu-command-removal-contribution';
 
 export default new ContainerModule((bind: interfaces.Bind) => {
     bind(MenuContribution).to(MenuCommandRemovalContribution).inSingletonScope();
