341c8dfa6c13bff22d855cbee751ae9cc193548b
midasa <samimitwalli@gmail.com>
2021-09-13 17:06:47 +0200

    removed pyro-stuff from cinco-cloud-theia

--- editor/web/pyro-model-server-extension/src/node/environmentVars.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable header/header */
-// Environment vars
-export const DATABASE_URL = process.env.DATABASE_URL ? process.env.DATABASE_URL : 'localhost:5432/quarkus_test';
-export const DATABASE_USER = process.env.DATABASE_USER ? process.env.DATABASE_USER : 'quarkus_test';
-export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : 'quarkus_test';
-export const PYRO_PORT = process.env.PYRO_PORT ? process.env.PYRO_PORT : '8000';
