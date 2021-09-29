e054d296f8cb379d8a2eff31c02ec42957d35657
Daniel Sami Mitwalli <samimitwalli@gmail.com>
2021-09-29 13:27:21 +0000

    Update runDocker.sh

--- archetype/runDocker.sh
+++ archetype/runDocker.sh
@@ -1,1 +1,1 @@
-docker run --env-file ./env.list --net=host -p 0.0.0.0:3000:3000 -p 0.0.0.0:8000:8000 --rm editor
+docker run --env-file ./env.list -p 0.0.0.0:3000:3000 -p 0.0.0.0:8000:8000 --rm editor
