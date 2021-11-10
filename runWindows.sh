8bd77e9a62eb1264bc20a8637b441dee8aa90006
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2021-11-10 11:17:41 +0100

    Delete runWindows.sh

--- runWindows.sh
+++
@@ -1,5 +0,0 @@
-#sh
-minikube start --cpus 4 --memory 8192 --driver=hyperv --disk-size 60000mb
-minikube addons enable default-storageclass && minikube addons enable ingress && minikube addons enable ingress-dns && minikube addons enable storage-provisioner
-kubectl apply -f secrets.yaml
-skaffold dev
