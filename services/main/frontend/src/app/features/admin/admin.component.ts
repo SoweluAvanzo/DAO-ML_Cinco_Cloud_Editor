3428f08791935c31627c623c72b63e8aac866cfb
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2022-03-08 12:47:09 +0100

    add subpages for the admin panel

--- services/main/frontend/src/app/features/admin/admin.component.ts
+++ services/main/frontend/src/app/features/admin/admin.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faCogs, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'cc-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  icons = {
+    users: faUsers,
+    cogs: faCogs
   }
-
 }
--- services/main/frontend/src/app/features/admin/admin.module.ts
+++ services/main/frontend/src/app/features/admin/admin.module.ts
@@ -2,19 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
-
+import { UsersComponent } from './pages/users/users.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { CoreModule } from '../../core/core.module';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  {
+    path: '',
+    redirectTo: 'users'
+  },
+  {
+    path: '',
+    pathMatch: 'exact',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'users',
+        component: UsersComponent
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    UsersComponent,
+    SettingsComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CoreModule
   ]
 })
 export class AdminModule { }
