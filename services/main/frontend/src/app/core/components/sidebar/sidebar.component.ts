426d9ce7c41969bbd3d719013b22f647e399d02f
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2022-03-14 15:32:55 +0100

    collapse sidebar

--- services/main/frontend/src/app/core/components/sidebar/sidebar.component.ts
+++ services/main/frontend/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UIState, UiStoreService } from '../../services/stores/ui-store.service';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'cc-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
-  collapsed: boolean = false
+  ui: UIState;
+
+  constructor(public uiStore: UiStoreService) {
+  }
+
+  ngOnInit(): void {
+    this.uiStore.ui$.subscribe({
+      next: ui => this.ui = ui
+    });
+  }
 
   get collapsedIcon() {
-    return this.collapsed ? 'angle-double-right' : 'angle-double-left';
+    return this.ui != null && this.ui.sidebarCollapsed ? 'angle-double-right' : 'angle-double-left';
   }
 }
