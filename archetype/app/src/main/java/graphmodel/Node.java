e8b83524e2a96ab9843cfb2f7b17c9d66633b0f8
Tagoukeng <joel.tagoukeng@tu-dortmund.de>
2022-03-18 16:29:39 +0100

    model element's extension of getter and setter

--- archetype/app/src/main/java/graphmodel/Node.java
+++ archetype/app/src/main/java/graphmodel/Node.java
@@ -4,9 +4,14 @@ public interface Node extends ModelElement {
 	
 	public void delete();
 	public int getX();
+	public void setX(int x);
 	public int getY();
+	public void setY(int y);
 	public int getWidth();
+	public void setWidth(int width);
 	public int getHeight();
+	public void setHeight(int height);
+
 
 	public java.util.List<Edge> getIncoming();
 	public <T extends Edge> java.util.List<T> getIncoming(Class<T> clazz);
