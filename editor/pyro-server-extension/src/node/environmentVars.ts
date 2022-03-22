6a61874288eb8b8621889dfdc6ce2415ed1a3a25
Alexander Bainczyk <alexander.bainczyk@udo.edu>
2022-03-22 11:41:56 +0000

    Remove hardcoded authentication url

--- editor/pyro-server-extension/src/node/environmentVars.ts
+++ editor/pyro-server-extension/src/node/environmentVars.ts
@@ -4,3 +4,6 @@ export const DATABASE_URL = process.env.DATABASE_URL ? process.env.DATABASE_URL 
 export const DATABASE_USER = process.env.DATABASE_USER ? process.env.DATABASE_USER : 'quarkus_test';
 export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : 'quarkus_test';
 export const PYRO_PORT = process.env.PYRO_PORT ? process.env.PYRO_PORT : '8000';
+export const CINCO_CLOUD_DEBUG = process.env.CINCO_CLOUD_DEBUG != null ? process.env.CINCO_CLOUD_DEBUG : 'false';
+export const CINCO_CLOUD_HOST = process.env.CINCO_CLOUD_HOST ? process.env.CINCO_CLOUD_HOST : '192.168.65.2';
+export const CINCO_CLOUD_PORT = process.env.CINCO_CLOUD_PORT ? process.env.CINCO_CLOUD_PORT : '3000';
