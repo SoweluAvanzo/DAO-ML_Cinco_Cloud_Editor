5dc5210d06ed225401dd8a427179a3702cb25e6d
Yannik Weber <yannik.weber@mcweber.de>
2022-04-06 10:18:58 +0000

    services/main: Display the progress of a build job in the editor

--- services/main/frontend/src/app/features/projects/pages/overview/components/status-widget/status-widget.component.ts
+++ services/main/frontend/src/app/features/projects/pages/overview/components/status-widget/status-widget.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Project } from '../../../../../../core/models/project';
 import { Page } from '../../../../../../core/models/page';
 import { WorkspaceImageBuildJob } from '../../../../../../core/models/workspace-image-build-job';
 import { ProjectStoreService } from '../../../../services/project-store.service';
 import { AppStoreService } from '../../../../../../core/services/stores/app-store.service';
-import { WorkspaceImageBuildJobApiService } from '../../../../../../core/services/api/workspace-image-build-job-api.service';
+import {
+  WorkspaceImageBuildJobApiService
+} from '../../../../../../core/services/api/workspace-image-build-job-api.service';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { filter, fromEvent } from 'rxjs';
 import { WebSocketMessage } from '../../../../../../core/models/web-socket-message';
 import { WebSocketEvent } from '../../../../../../core/enums/web-socket-event';
 import { fromJsog } from '../../../../../../core/utils/jsog-utils';
+import { ToastService, ToastType } from '../../../../../../core/services/toast.service';
 
 @UntilDestroy()
 @Component({
   selector: 'cc-status-widget',
   templateUrl: './status-widget.component.html'
 })
-export class StatusWidgetComponent implements OnInit {
+export class StatusWidgetComponent implements OnInit, OnDestroy {
 
   @Input()
   project: Project;
@@ -25,21 +28,18 @@ export class StatusWidgetComponent implements OnInit {
 
   constructor(private projectStore: ProjectStoreService,
               private appStore: AppStoreService,
+              private toastService: ToastService,
               private buildJobApi: WorkspaceImageBuildJobApiService) {
   }
 
   ngOnInit(): void {
-    this.buildJobApi.getAll(this.project.id, 1, 1).subscribe({
+    this.buildJobApi.getAll(this.project.id, 0, 1).subscribe({
       next: (test: Page<WorkspaceImageBuildJob>) => {
-        this.job = test.items.sort((job1, job2) => {
-          if (job1.startedAt > job2.startedAt) {
-            return 1;
-          }
-          if (job1.startedAt < job2.startedAt) {
-            return -1;
-          }
-          return 0;
-        })[0];
+        if (test.items.length < 1) {
+          this.job = null;
+        } else {
+          this.job = test.items[0]
+        }
       }
     });
 
@@ -64,6 +64,10 @@ export class StatusWidgetComponent implements OnInit {
         }
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.projectStore.closeWebSocket();
   }
 
 }
