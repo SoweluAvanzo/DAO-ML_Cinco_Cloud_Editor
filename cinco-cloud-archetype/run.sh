99afdaacdee64da0116ed5768cf4cf5f7bca3da1
midasa <samimitwalli@gmail.com>
2022-07-11 13:58:02 +0200

    fix run scripts

--- cinco-cloud-archetype/run.sh
+++ cinco-cloud-archetype/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --env-file ./env.list --add-host=host.docker.internal:host-gateway -p 0.0.0.0:3000:3000 -p 0.0.0.0:8000:8000 --rm -i editor
+exec docker run --env-file ./env.list --add-host=host.docker.internal:host-gateway -p 0.0.0.0:3000:3000 -p 0.0.0.0:8000:8000 --rm -i editor
