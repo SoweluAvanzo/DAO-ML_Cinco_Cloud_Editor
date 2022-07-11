28b05540e32e3df68ec68e573a14636696835279
midasa <samimitwalli@gmail.com>
2022-07-11 13:35:00 +0200

    add docker-script changes

--- cinco-cloud-archetype/build.sh
+++ cinco-cloud-archetype/build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+# copy language-server sources
+cp -r ../cinco-language-server/ cinco-language-server/
+# remove emf-workspace
+rm -r cinco-language-server/workspace-emf-tmp
+
 docker build -t editor .
--- cinco-cloud-archetype/run.sh
+++ cinco-cloud-archetype/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec docker run --env-file ./env.list --add-host=host.docker.internal:host-gateway -p 0.0.0.0:3000:3000 -p 0.0.0.0:8000:8000 --rm -it editor
+docker run --env-file ./env.list --add-host=host.docker.internal:host-gateway -p 0.0.0.0:3000:3000 -p 0.0.0.0:8000:8000 --rm -i editor
