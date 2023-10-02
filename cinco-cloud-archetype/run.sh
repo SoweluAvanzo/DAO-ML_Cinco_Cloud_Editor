a4b16a883d88d7e0e937be38f33e2cd63be992fe
Daniel Busch <daniel2.busch@tu-dortmund.de>
2023-10-02 17:51:28 +0200

    Fix access rights for Dockerfile and associated run script

--- cinco-cloud-archetype/run.sh
+++ cinco-cloud-archetype/run.sh
@@ -4,7 +4,6 @@ exec docker run \
     --env-file ./env.list \
     --add-host=host.docker.internal:host-gateway \
     --volume=./editor/workspace:/editor/workspace \
-    --user=node \
     -p 0.0.0.0:3000:3000 \
     -p 0.0.0.0:8000:8000 \
     --rm \
