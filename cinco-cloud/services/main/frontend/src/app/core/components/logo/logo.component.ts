655ab716aff8b36372b09450524984e66cef132a
Alexander Bainczyk <alexander.bainczyk@udo.edu>
2023-10-10 14:50:09 +0000

    Update logos and align spelling of Cinco Cloud

---
+++ cinco-cloud/services/main/frontend/src/app/core/components/logo/logo.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'cc-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss']
+})
+export class LogoComponent {
+
+  @Input()
+  height: string;
+}
