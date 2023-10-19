fa74b132c9ba3e3cb98e52679aa85bf514369cfb
midasa <samimitwalli@gmail.com>
2023-10-19 13:44:51 +0200

    add feedback for mgl generation

--- cinco-cloud-archetype/vscode-extensions/cinco-languages-extension/src/mgl/cli/persistence_handler.ts
+++ cinco-cloud-archetype/vscode-extensions/cinco-languages-extension/src/mgl/cli/persistence_handler.ts
@@ -22,27 +22,32 @@ interface ServerArgs {
  * @returns the path to the languagesFolder
  */
 export function saveToLanguagesFolder(content: string, fileName: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        vscode.commands.executeCommand( 'cinco.provide.glsp-server-args').then( result => {
-            const serverArgs = result as ServerArgs;
-            const languagesFolder = serverArgs.rootFolder + '/' + serverArgs.languagePath;
-            // create if not existing
-            if (!fs.existsSync(languagesFolder)) {
-                fs.mkdirSync(languagesFolder, { recursive: true });
-            }
-            const targetPath = languagesFolder + '/' + fileName;
-            console.log('Integrating meta-specification to: '+ targetPath);
-            fs.writeFileSync(targetPath, content);
-            resolve(languagesFolder);
-            //  update meta-specification in glsp-server
-            vscode.commands.executeCommand('cinco.meta-specification.reload');
-        }).then(() => {}, (e) => {
-            reject(e)
-        })
+    return new Promise<string>(async (resolve, reject) => {
+        let serverArgs:  ServerArgs;
+        try {
+            serverArgs = await vscode.commands.executeCommand( 'cinco.provide.glsp-server-args');
+        } catch(e) {
+            serverArgs = {
+                languagePath: "workspace/languages",
+                rootFolder: __dirname + "/../../../../../editor",
+            } as ServerArgs;
+
+        }
+        const languagesFolder = serverArgs.rootFolder + '/' + serverArgs.languagePath;
+        // create if not existing
+        if (!fs.existsSync(languagesFolder)) {
+            fs.mkdirSync(languagesFolder, { recursive: true });
+        }
+        const targetPath = languagesFolder + '/' + fileName;
+        console.log('Integrating meta-specification to: '+ targetPath);
+        fs.writeFileSync(targetPath, content);
+        resolve(languagesFolder);
+        //  update meta-specification in glsp-server
+        vscode.commands.executeCommand('cinco.meta-specification.reload');
     });
 }
 
-export function uploadToMinio(directoryPath: string): void {
+export function uploadToMinio(directoryPath: string, onError: (e: any) => void): Promise<void> {
     // collect and zip files
     const zip = new AdmZip();
     // Get items in the directory
@@ -58,14 +63,16 @@ export function uploadToMinio(directoryPath: string): void {
 
     // upload files to minio
     const minioClient: Client = createClient();
-    minioClient.putObject('projects', MINIO_RESOURCE_ID + '.zip', zip.toBuffer()).then(success => {
+    return minioClient.putObject('projects', MINIO_RESOURCE_ID + '.zip', zip.toBuffer()).then(success => {
         console.log('Stored meta specification successfully');
         client.createBuildJob(new CreateBuildJobMessage().setProjectid(parseInt(MINIO_RESOURCE_ID)), (error, response) => {
             if(error) {
-                console.error('Failed to submit create build job');
+                throw Error('Failed to submit build job:\n'+error);
             }
+            onError(error);
         });
-    }).catch(error =>
-        console.log('Unable to store meta specification', error)
-    );
+    }).catch(error => {
+        onError(error);
+        throw Error('Failed to submit create build job:\n'+error);
+    });
 }
