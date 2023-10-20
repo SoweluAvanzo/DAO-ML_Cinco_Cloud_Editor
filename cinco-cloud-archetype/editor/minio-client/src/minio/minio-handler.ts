befec2d2a1b3c68444028df164869d3e0540fd89
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2023-10-20 20:25:49 +0000

    Initialize workspaces of model editors

--- cinco-cloud-archetype/editor/minio-client/src/minio/minio-handler.ts
+++ cinco-cloud-archetype/editor/minio-client/src/minio/minio-handler.ts
@@ -15,9 +15,12 @@
  ********************************************************************************/
 
 import * as path from 'path';
+import * as fs from 'fs';
 import { createClient } from './minio-client';
 import { MINIO_RESOURCE_ID } from './minio_values';
 import AdmZip = require('adm-zip');
+
+const WORKSPACE_INITIALIZATION_DIRECTORY = 'workspace';
 
 export function fetchMetaSpecification(targetFolder: string): void {
     if (MINIO_RESOURCE_ID !== '') {
@@ -29,10 +32,49 @@ export function fetchMetaSpecification(targetFolder: string): void {
             const zip = new AdmZip(targetPath);
             zip.extractAllTo(targetFolder, true);
             console.log('Fetched meta specification successfully');
+            
+            // Check if WORKSPACE_INITIALIZATION_DIRECTORY exists
+            const workspacePath = path.join(targetFolder, WORKSPACE_INITIALIZATION_DIRECTORY);
+            const editorWorkspacePath = process.env.WORKSPACE_PATH;
+
+            if (fs.existsSync(workspacePath)) {
+                if (isDirectoryEmpty(editorWorkspacePath)) {
+                    copyFolderRecursiveSync(workspacePath, editorWorkspacePath);
+                    console.log(`Files from ${workspacePath} copied to ${editorWorkspacePath}`);
+
+                    // Delete the workspacePath folder
+                    fs.rmdirSync(workspacePath, { recursive: true });
+                } else {
+                    console.log(`${editorWorkspacePath} is not empty. Skipping initialization.`);
+                }
+            } else {
+                console.log(`${workspacePath} does not exist. Skipping initialization.`);
+            }
         }).catch(e => {
             console.log('error fetching fetching meta-specification!');
         });
     } else {
         console.log('MINIO_RESOURCE_ID not set!');
     }
+}
+
+function isDirectoryEmpty(directory: string): boolean {
+    return fs.readdirSync(directory).length === 0;
+}
+
+function copyFolderRecursiveSync(source: string, target: string) {
+    const files = fs.readdirSync(source);
+    files.forEach(file => {
+        const sourcePath = path.join(source, file);
+        const targetPath = path.join(target, file);
+
+        if (fs.statSync(sourcePath).isDirectory()) {
+            if (!fs.existsSync(targetPath)) {
+                fs.mkdirSync(targetPath);
+            }
+            copyFolderRecursiveSync(sourcePath, targetPath);
+        } else {
+            fs.copyFileSync(sourcePath, targetPath);
+        }
+    });
 }
