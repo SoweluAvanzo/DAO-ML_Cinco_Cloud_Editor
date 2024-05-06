736ad6fa9892fa6facdc97a5cb2e5d757fcac9d3
midasa <samimitwalli@gmail.com>
2024-05-06 06:27:34 +0200

    add license

--- cinco-cloud-archetype/vscode-extensions/cinco-languages-extension/src/mgl/language-server/mgl-validator.ts
+++ cinco-cloud-archetype/vscode-extensions/cinco-languages-extension/src/mgl/language-server/mgl-validator.ts
@@ -1,4 +1,19 @@
-import { /*ValidationAcceptor,*/ ValidationAcceptor, ValidationChecks } from 'langium';
+/********************************************************************************
+ * Copyright (c) 2024 Cinco Cloud.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+import { ValidationAcceptor, ValidationChecks } from 'langium';
 import { CincoAstType, Edge, EdgeElementConnection, ModelElement, ComplexModelElement, NodeType } from '../../generated/ast';
 import type { MglServices } from './mgl-module';
 import { getConnectingEdges } from '../util/mgl-util';
