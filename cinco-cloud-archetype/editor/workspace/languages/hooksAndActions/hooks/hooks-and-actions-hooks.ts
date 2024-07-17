b450849711710a11d95a457c15d17dfe42450e32
midasa <samimitwalli@gmail.com>
2024-07-17 15:25:05 +0200

    add examples and tests for other missing hooks

--- cinco-cloud-archetype/editor/workspace/languages/hooksAndActions/hooks/hooks-and-actions-hooks.ts
+++ cinco-cloud-archetype/editor/workspace/languages/hooksAndActions/hooks/hooks-and-actions-hooks.ts
@@ -13,35 +13,53 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
- import { Node, LanguageFilesRegistry, Container, AbstractGraphModelHook, GraphModel } from '@cinco-glsp/cinco-glsp-api';
+ import { LanguageFilesRegistry, AbstractGraphModelHook, GraphModel } from '@cinco-glsp/cinco-glsp-api';
  import { PropertyEditOperation, AssignValue } from '@cinco-glsp/cinco-glsp-common';
- import { CreateNodeOperation, Dimension, Point } from '@eclipse-glsp/server';
  
- export class HooksAndActionsHook extends AbstractGraphModelHook {
-     override CHANNEL_NAME: string | undefined = 'HooksAndActionsHook';
- 
-     /**
-      * Create
-      */
+export class HooksAndActionsHook extends AbstractGraphModelHook {
+    override CHANNEL_NAME: string | undefined = 'HooksAndActionsHook';
+
+    /**
+     * Create
+     */
 
     override canCreate(modelElementType: string, path: string): boolean {
         this.log('Triggered canCreate. Can create model of type (' + modelElementType + ') at path (' + path + ')');
         return true;
     }
-    
+
     override preCreate(modelElementType: string, path: string): void {
         this.log('Triggered preCreate. Creating model of type (' + modelElementType + ') at path (' + path + ')');
     }
- 
+
     override postCreate(graphModel: GraphModel): void {
         this.log('Triggered postCreate on graphmodel (' + graphModel.id + ')');
+    }
+
+    /**
+     * Delete - TODO
+     */
+
+     override canDelete(graphModel: GraphModel): boolean {
+        this.log('Triggered canDelete on graphModel (' + graphModel.id + ')');
+        return true;
+    }
+
+    override preDelete(graphModel: GraphModel): boolean {
+        this.log('Triggered preDelete on graphModel (' + graphModel.id + ')');
+        return true;
+    }
+
+    override postDelete(graphModel: GraphModel): boolean {
+        this.log('Triggered postDelete on graphModel (' + graphModel.id + ')');
+        return true;
     }
 
     /**
      * Attribute Change
      */
 
-     override canAttributeChange(graphModel: GraphModel, operation: PropertyEditOperation): boolean {
+    override canAttributeChange(graphModel: GraphModel, operation: PropertyEditOperation): boolean {
         this.log('Triggered canAttributeChange on graphModel (' + graphModel.id + ')');
         return operation.change.kind === 'assignValue';
     }
@@ -62,7 +80,46 @@
         this.log('Triggered postAttributeChange on graphModel (' + graphModel.id + ')');
         this.log('Changed: ' + attributeName + ' from: ' + oldValue + ' to: ' + graphModel.getProperty(attributeName));
     }
- }
+
+    /**
+     * Select - TODO
+     */
+
+    override canSelect(graphModel: GraphModel, isSelected: boolean): boolean {
+        this.log('Triggered canSelect on graphModel (' + graphModel.id + ') - selected: ' + isSelected);
+        return true;
+    }
+
+    override postSelect(graphModel: GraphModel, isSelected: boolean): boolean {
+        this.log('Triggered postSelect on graphModel (' + graphModel.id + ') - selected: ' + isSelected);
+        return true;
+    }
+
+    /**
+     * Double Click - TODO
+     */
+
+    override canDoubleClick(graphModel: GraphModel): boolean {
+        this.log('Triggered canDoubleClick on graphModel (' + graphModel.id + ')');
+        return true;
+    }
+
+    override postDoubleClick(graphModel: GraphModel): void {
+        this.log('Triggered postDoubleClick on graphModel (' + graphModel.id + ')');
+    }
+
+    /**
+     * Save - TODO
+     */
+
+    preSave(graphModel: GraphModel): void {
+        this.log('Triggered preSave on graphModel (' + graphModel.id + ')');
+    }
+
+    postSave(graphModel: GraphModel): void {
+        this.log('Triggered postSave on graphModel (' + graphModel.id + ')');
+    }
+}
  
- LanguageFilesRegistry.register(HooksAndActionsHook);
+LanguageFilesRegistry.register(HooksAndActionsHook);
  
