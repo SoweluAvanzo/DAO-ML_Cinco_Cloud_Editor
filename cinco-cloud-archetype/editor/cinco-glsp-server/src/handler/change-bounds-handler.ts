a8465e5c791e9adfee9efbba416c854cb971ad6e
Fabian Storek <3779252-Jochel@users.noreply.gitlab.com>
2024-07-30 12:57:12 +0200

    Don't trigger move hooks when resizing

--- cinco-cloud-archetype/editor/cinco-glsp-server/src/handler/change-bounds-handler.ts
+++ cinco-cloud-archetype/editor/cinco-glsp-server/src/handler/change-bounds-handler.ts
@@ -23,6 +23,7 @@ import { HookManager } from '@cinco-glsp/cinco-glsp-api';
 
 @injectable()
 export class ChangeBoundsHandler extends CincoJsonOperationHandler {
+
     readonly operationType = ChangeBoundsOperation.KIND;
 
     override executeOperation(operation: ChangeBoundsOperation): void {
@@ -36,68 +37,54 @@ export class ChangeBoundsHandler extends CincoJsonOperationHandler {
         const node = index.findByClass(elementId, GNode);
         const nodeObj = node ? index.findNode(node.id) : undefined;
         if (nodeObj) {
-            if (newPosition && this.isMove(nodeObj.position, newPosition)) {
+            const oldSize = nodeObj.size;
+            const oldPosition = nodeObj.position;
+            const isResize = oldSize && newSize && (oldSize.width !== newSize.width || oldSize.height !== newSize.height);
+            const isMove = !isResize && oldPosition && newPosition && !Point.equals(oldPosition, newPosition);
+            if (isResize) {
+                this.handleResize(nodeObj, newSize, newPosition);
+            } else if (isMove) {
                 this.handleMove(nodeObj, newPosition);
             }
-            if (newSize && this.isResize(nodeObj.size, newSize)) {
-                this.handleResize(nodeObj, newSize);
-            }
         }
     }
 
-    private handleMove(node: Node, newPosition: Point | undefined): void {
+    private handleResize(node: Node, newSize: Dimension, newPosition: Point | undefined): void {
+        const oldSize = node.size;
         const oldPosition = node.position;
-        if (!newPosition || !this.isMove(oldPosition, newPosition)) {
-            return;
+        const parameters: ResizeArgument = {
+            kind: 'Resize',
+            modelElementId: node.id,
+            oldSize: oldSize,
+            newSize: newSize,
+            oldPosition: oldPosition,
+            newPosition: newPosition ?? oldPosition
+        };
+        const canResize = HookManager.executeHook(parameters, HookType.CAN_RESIZE, this.modelState, this.logger, this.actionDispatcher);
+        if (canResize) {
+            HookManager.executeHook(parameters, HookType.PRE_RESIZE, this.modelState, this.logger, this.actionDispatcher);
+            if (newPosition) {
+                node.position = newPosition;
+            }
+            node.size = newSize;
+            HookManager.executeHook(parameters, HookType.POST_RESIZE, this.modelState, this.logger, this.actionDispatcher);
         }
-        const moveParameters: MoveArgument = {
+    }
+
+    private handleMove(node: Node, newPosition: Point): void {
+        const oldPosition = node.position;
+        const parameters: MoveArgument = {
             kind: 'Move',
             modelElementId: node.id,
-            oldPosition: node.position,
+            oldPosition: oldPosition,
             newPosition: newPosition
         };
-        if (newPosition && this.canMove(moveParameters)) {
-            HookManager.executeHook(moveParameters, HookType.PRE_MOVE, this.modelState, this.logger, this.actionDispatcher);
+        const canMove = HookManager.executeHook(parameters, HookType.CAN_MOVE, this.modelState, this.logger, this.actionDispatcher);
+        if (canMove) {
+            HookManager.executeHook(parameters, HookType.PRE_MOVE, this.modelState, this.logger, this.actionDispatcher);
             node.position = newPosition;
-            HookManager.executeHook(moveParameters, HookType.POST_MOVE, this.modelState, this.logger, this.actionDispatcher);
+            HookManager.executeHook(parameters, HookType.POST_MOVE, this.modelState, this.logger, this.actionDispatcher);
         }
     }
 
-    private handleResize(node: Node, newSize: Dimension | undefined): void {
-        const oldSize = node.size;
-        if (!newSize || !oldSize || !this.isResize(oldSize, newSize)) {
-            return;
-        }
-        if (newSize !== node.size) {
-            const parameters: ResizeArgument = {
-                kind: 'Resize',
-                modelElementId: node.id,
-                oldSize: node.size,
-                newSize: newSize,
-                oldPosition: node.position,
-                newPosition: node.position // TODO: This needs to change (For @Jochel)
-            };
-            if (this.canResize(newSize, parameters)) {
-                HookManager.executeHook(parameters, HookType.PRE_RESIZE, this.modelState, this.logger, this.actionDispatcher);
-                node.size = newSize;
-                HookManager.executeHook(parameters, HookType.POST_RESIZE, this.modelState, this.logger, this.actionDispatcher);
-            }
-        }
-    }
-
-    private isMove(oldPosition: Point, newPosition: Point): boolean {
-        return !Point.equals(oldPosition, newPosition);
-    }
-
-    private isResize(oldDimension: Dimension, newDimension: Dimension): boolean {
-        return oldDimension.width !== newDimension.width || oldDimension.height !== newDimension.height;
-    }
-
-    private canMove(parameters: MoveArgument): boolean {
-        return HookManager.executeHook(parameters, HookType.CAN_MOVE, this.modelState, this.logger, this.actionDispatcher);
-    }
-
-    private canResize(newSize: Dimension, parameters: ResizeArgument): boolean {
-        return HookManager.executeHook(parameters, HookType.CAN_RESIZE, this.modelState, this.logger, this.actionDispatcher);
-    }
 }
