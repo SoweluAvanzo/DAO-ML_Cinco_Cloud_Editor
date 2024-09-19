3ad9478bc568b6313be78d8cb805f949d341a98b
midasa <samimitwalli@gmail.com>
2024-09-19 16:46:56 +0200

    remove old cinco-languages-extension

--- cinco-cloud-archetype/vscode-extensions/cinco-languages-extension/src/grpc/grpc-client.ts
+++
@@ -1,4 +0,0 @@
-import * as grpc from '@grpc/grpc-js';
-import { MainServiceClient } from '../../generated/cinco-cloud_grpc_pb';
-
-export const client = new MainServiceClient(`${process.env.CINCO_CLOUD_HOST}:9000`, grpc.credentials.createInsecure());
