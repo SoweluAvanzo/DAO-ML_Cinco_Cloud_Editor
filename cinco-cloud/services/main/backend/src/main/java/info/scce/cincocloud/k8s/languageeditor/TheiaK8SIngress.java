3cb4e10ffa70b02b3d3d7134e7275d2e069324c9
Marco Krumrey <marco.krumrey@tu-dortmund.de>
2024-10-08 17:17:00 +0200

    Increase the max allowed upload size for files to 8mb

--- cinco-cloud/services/main/backend/src/main/java/info/scce/cincocloud/k8s/languageeditor/TheiaK8SIngress.java
+++ cinco-cloud/services/main/backend/src/main/java/info/scce/cincocloud/k8s/languageeditor/TheiaK8SIngress.java
@@ -51,7 +51,8 @@ public class TheiaK8SIngress extends TheiaK8SResource<Ingress> {
             "nginx.ingress.kubernetes.io/add-base-url", "true",
             "nginx.ingress.kubernetes.io/rewrite-target", "/$2",
             "nginx.ingress.kubernetes.io/proxy-read-timeout", "3600",
-            "nginx.ingress.kubernetes.io/proxy-send-timeout", "3600"))
+            "nginx.ingress.kubernetes.io/proxy-send-timeout", "3600",
+            "nginx.ingress.kubernetes.io/proxy-body-size", "8m"))
         .endMetadata()
         .withSpec(new IngressSpecBuilder()
             .withRules(new IngressRuleBuilder()
