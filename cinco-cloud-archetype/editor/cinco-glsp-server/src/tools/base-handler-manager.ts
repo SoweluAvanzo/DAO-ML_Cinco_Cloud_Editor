2d81c69e85f6819d0ee726fe484b34a9f910dd9f
midasa <samimitwalli@gmail.com>
2024-11-11 15:49:47 +0100

    streamline basehandlermanager

--- cinco-cloud-archetype/editor/cinco-glsp-server/src/tools/base-handler-manager.ts
+++ cinco-cloud-archetype/editor/cinco-glsp-server/src/tools/base-handler-manager.ts
@@ -63,62 +63,40 @@ export abstract class BaseHandlerManager<A extends ManagedBaseAction, H extends 
     // executes handler under the given context
     abstract executeHandler(handler: H, element: ModelElement, action: A, args: any): Promise<Action[]> | Action[];
 
-    execute(action: A, ...args: unknown[]): Promise<Action[]> {
-        // Is not a user client?
-        if ([GLSP_TEMP_ID].includes(this.modelState.clientId) || !this.modelState.graphModel) {
+    async execute(action: A, ...args: unknown[]): Promise<Action[]> {
+        if (
+            // Is not a user client?
+            [GLSP_TEMP_ID].includes(this.modelState.clientId) ||
+            // not the associated graphmodel
+            !this.modelState.graphModel ||
+            ('modelId' in action && action.modelId !== this.modelState.graphModel?.id)
+        ) {
             return Promise.resolve([]);
         }
-        if ('modelId' in action) {
-            if (action.modelId !== this.modelState.graphModel?.id) {
-                // not the associated graphmodel
-                return Promise.resolve([]);
-            }
+
+        const result: Action[] = [];
+        const handlers = await this.getActiveHandlers(action, args);
+        if (handlers.length <= 0) {
+            return result;
         }
-        return new Promise<Action[]>((resolve, _) => {
-            const results: Action[] = [];
-            this.getActiveHandlers(action, args).then(handlers => {
-                if (handlers.length <= 0) {
-                    resolve([]);
+
+        console.log(handlers.length + ' handlers will be executed as ' + this.baseHandlerName + '...');
+        result.push(SaveModelAction.create());
+        await Promise.all(
+            handlers.map(async handler => {
+                console.log(handler?.constructor.name + ' handler will be executed...');
+                const element = this.modelState.index.findElement(action.modelElementId) as ModelElement;
+                try {
+                    const executedResult = await this.executeHandler(handler, element, action, args);
+                    result.push(...executedResult);
+                } catch (e) {
+                    console.log(`Error executing handler: ${handler?.constructor.name}`);
+                    this.notify(`${handler.constructor.name} ran into errors!`, 'ERROR');
+                    console.log(`${e}`);
                 }
-                let leftToHandle: number = handlers.length;
-                console.log(leftToHandle + ' handlers will be executed as ' + this.baseHandlerName + '...');
-                handlers.forEach(handler => {
-                    console.log(handler?.constructor.name + ' handler will be executed...');
-                    const element = this.modelState.index.findElement(action.modelElementId) as ModelElement;
-                    try {
-                        const result = this.executeHandler(handler, element, action, args);
-                        if (result instanceof Promise) {
-                            result.then((v: Action[]) => {
-                                results.push(...v);
-                                leftToHandle = leftToHandle - 1;
-                                if (leftToHandle <= 0) {
-                                    // save model action
-                                    results.push(SaveModelAction.create());
-                                    resolve(results);
-                                }
-                            });
-                        } else {
-                            results.push(...result);
-                            leftToHandle = leftToHandle - 1;
-                            if (leftToHandle <= 0) {
-                                // save model action
-                                resolve(results);
-                            }
-                        }
-                    } catch (e) {
-                        console.log(`Error executing handler: ${handler?.constructor.name}`);
-                        this.notify(`${handler.constructor.name} ran into errors!`, 'ERROR');
-                        console.log(`${e}`);
-                        leftToHandle = leftToHandle - 1;
-                        if (leftToHandle <= 0) {
-                            // save model action
-                            results.push(SaveModelAction.create());
-                            resolve(results);
-                        }
-                    }
-                });
-            });
-        });
+            })
+        );
+        return result;
     }
 
     /**
@@ -145,6 +123,54 @@ export abstract class BaseHandlerManager<A extends ManagedBaseAction, H extends 
             console.log(`${e}`);
             return Promise.resolve([]);
         }
+        const applicableHandlerClasses = await this.getApplicableHandlers(element);
+        return this.getExecutableHandlers(applicableHandlerClasses, element, action, args);
+    }
+
+    async getExecutableHandlers(applicableHandlerClasses: any[], element: ModelElement, action: A, ...args: unknown[]): Promise<H[]> {
+        const actionHandlers: H[] = [];
+        const promisedProcedure: Promise<void>[] = [];
+        console.log('[' + applicableHandlerClasses.length + '] handlers will be tested for execution as a ' + this.baseHandlerName + '!');
+        for (const handlerClass of applicableHandlerClasses) {
+            try {
+                // initialize handler
+                const handler = new handlerClass(
+                    this.logger,
+                    this.modelState,
+                    this.actionDispatcher,
+                    this.sourceModelStorage,
+                    this.submissionHandler
+                );
+                // test if handler can be executed
+                const canExecute = this.handlerCanBeExecuted(handler, element, action, args);
+                if (canExecute instanceof Promise) {
+                    // mark handler as active/executable
+                    promisedProcedure.push(
+                        canExecute.then(value => {
+                            if (value) {
+                                console.log('[' + handlerClass.name + '] can be executed as a ' + this.baseHandlerName + '!');
+                                actionHandlers.push(handler);
+                            }
+                        })
+                    );
+                } else {
+                    // mark handler as active/executable
+                    if (canExecute) {
+                        console.log('[' + handlerClass.name + '] can be executed as a ' + this.baseHandlerName + '!');
+                        actionHandlers.push(handler);
+                    }
+                }
+            } catch (e) {
+                console.log(`Error checking executability of: ${handlerClass.name}`);
+                this.notify(`Error checking executability of: ${handlerClass.name}`, 'ERROR');
+                console.log(`${e}`);
+            }
+        }
+        await Promise.all(promisedProcedure);
+        return actionHandlers;
+    }
+
+    async getApplicableHandlers(element: ModelElement): Promise<H[]> {
         const applicableHandlerClasses = await BaseHandlerManager.getHandlerClasses(
             this.baseHandlerName,
             (handlerClassName: string): boolean => {
@@ -165,56 +191,7 @@ export abstract class BaseHandlerManager<A extends ManagedBaseAction, H extends 
             );
             return Promise.resolve([]);
         }
-        return new Promise<H[]>((resolve, _) => {
-            let leftToHandle: number = applicableHandlerClasses.length;
-            const actionHandlers: H[] = [];
-            console.log('[' + leftToHandle + '] handlers will be tested for execution as a ' + this.baseHandlerName + '!');
-            for (const handlerClass of applicableHandlerClasses) {
-                try {
-                    // initialize handler
-                    const handler = new handlerClass(
-                        this.logger,
-                        this.modelState,
-                        this.actionDispatcher,
-                        this.sourceModelStorage,
-                        this.submissionHandler
-                    );
-                    // test if handler can be executed
-                    const canExecute = this.handlerCanBeExecuted(handler, element, action, args);
-                    if (canExecute instanceof Promise) {
-                        // mark handler as active/executable
-                        canExecute.then(value => {
-                            if (value) {
-                                console.log('[' + handlerClass.name + '] can be executed as a ' + this.baseHandlerName + '!');
-                                actionHandlers.push(handler);
-                            }
-                            leftToHandle = leftToHandle - 1;
-                            if (leftToHandle <= 0) {
-                                resolve(actionHandlers);
-                            }
-                        });
-                    } else {
-                        // mark handler as active/executable
-                        if (canExecute) {
-                            console.log('[' + handlerClass.name + '] can be executed as a ' + this.baseHandlerName + '!');
-                            actionHandlers.push(handler);
-                        }
-                        leftToHandle = leftToHandle - 1;
-                        if (leftToHandle <= 0) {
-                            resolve(actionHandlers);
-                        }
-                    }
-                } catch (e) {
-                    console.log(`Error checking executability of: ${handlerClass.name}`);
-                    this.notify(`Error checking executability of: ${handlerClass.name}`, 'ERROR');
-                    console.log(`${e}`);
-                    leftToHandle = leftToHandle - 1;
-                    if (leftToHandle <= 0) {
-                        resolve(actionHandlers);
-                    }
-                }
-            }
-        });
+        return applicableHandlerClasses;
     }
 
     /**
