97d3048a8c08cf3ddcf4e05c6c007a93aaa14678
SoweluAvanzo <soweluelios.avanzo@unito.it>
2025-01-21 11:12:22 +0100

    adding use cases
    Signed-off-by: SoweluAvanzo <soweluelios.avanzo@unito.it>

---
+++ editor/workspace/output/Decentralized_Destination_Management_Organization/DDMO_Council.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// @title DdmoCouncil in Decentralized_Destination_Management_Organization, using the None protocol
+
+
+ 
+
+contract DdmoCouncil  {
+    
+    // TODO: implement custom None logic here
+
+     
+    // Constructor
+    constructor( )
+    {
+         
+    }  
+}
